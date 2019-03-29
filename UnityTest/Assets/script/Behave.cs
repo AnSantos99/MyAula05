2adfb88ef4703bb3f12306deb4e9f4313c1c127c
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-29 12:19:32 +0000

    Projeto unity com gitignore e attributes

---
+++ UnityTest/Assets/script/Behave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("MEEEEOW!");
+        transform.localScale = new Vector3 (3, 3, 3);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rot = new Vector3(0.5f, 0.5f, 0.5f);
+        transform.Rotate(rot);
+    }
+}
