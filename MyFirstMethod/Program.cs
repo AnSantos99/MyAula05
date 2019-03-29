a15c8f7c696a4c4853c4cea43f7f44935300bde3
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-29 11:18:26 +0000

    XMl Documentation of Methods

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,16 +4,35 @@ namespace MyFirstMethod
 {
     class Program
     {
+        /// <summary>
+        /// Method declaration static void (no return) with
+        /// the signature CountToN. The body contains a
+        /// for loop that counts from 1 to n times.
+        /// </summary>
+        /// <param name="n"> variable of type intenger</param>
         static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
                 Console.WriteLine(i);
         }
+
+        /// <summary>
+        /// Method declaration static void (no arguments and no return) 
+        /// with the CountTo10 signature. The body contains Method CountToN
+        /// (with the given value of 10 thats assigned to variable `n`)
+        /// </summary>
         static void CountTo10()
         {
             CountToN(10);
         }
          
+        /// <summary>
+        /// Main method of the main program. The declaration is static void
+        /// (no return) with the signature Main containing 1 argument
+        /// (string [] args).
+        /// The body contains the method CountToN 2 times and CountTo10
+        /// </summary>
+        /// <param name="args"></param>
     static void Main(string[] args)
         {
             CountToN(4);
